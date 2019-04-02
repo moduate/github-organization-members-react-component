027903f8473759c907585c7b93b735bbe43171b8
Jason-Cooke <jason.b.cooke@gmail.com>
2019-04-02 19:59:23 +1300

    Add Member class component

---
+++ src/index.js
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+// import ReactDOM from 'react-dom';
+
+class Member extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <div></div>
+    )
+  }
+}
+
+export default Member;
