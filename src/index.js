0b740cd696d3751639f377b59043a9d5d8c5aa61
Jason-Cooke <jason.b.cooke@gmail.com>
2019-04-04 20:05:33 +1300

    Convert to form and add handleSubmit function

--- src/index.js
+++ src/index.js
@@ -11,18 +11,30 @@ class Member extends Component {
     this.setState({ organization: event.target.value })
   }
 
+  handleSubmit = event => {
+    event.preventDefault();
+    console.log(gom.getPublicUsersFromOrg(this.state.organization, (err, result) => {
+      if(err) { console.log(err) }
+      console.log(result);
+    }))
+  }
+
   render() {
     return (
       <div style={{padding: '20px 10px'}}>
-        <label>
-          Enter Organization
-          <input
-            style={{ marginLeft: 5 }}
-            value={this.state.organization}
-            onChange={this.updateOrganization}
-          >
-          </input>
-        </label>
+        <form onSubmit={this.handleSubmit}>
+          <label>
+            Enter Organization
+            <input
+              style={{ marginLeft: 5 }}
+              type="text"
+              value={this.state.organization}
+              onChange={this.updateOrganization}
+              >
+            </input>
+          </label>
+          <button>Submit</button>
+        </form>
       </div>
     )
   }
