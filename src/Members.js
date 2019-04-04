30d1da995a6c87902d65c7407a8f48f2261d4bc8
Jason-Cooke <jason.b.cooke@gmail.com>
2019-04-05 11:08:26 +1300

    Create Members component

---
+++ src/Members.js
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+
+class Members extends Component {
+  constructor(props) {
+    super(props);
+  }
+
+  render() {
+    return (
+      <ul></ul>
+    )
+  }
+}
+
+export default Members;
